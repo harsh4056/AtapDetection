81fe8107c9f953b17d04590d759d811a1c3c2c08
moon <hs4056@gmail.com>
2017-06-30 21:02:00 +0530

    Zscore thresholding algorithm added With documentation

--- app/src/main/java/com/example/moon/atapdetection/RealtimeDataDisplayFragment.java
+++ app/src/main/java/com/example/moon/atapdetection/RealtimeDataDisplayFragment.java
@@ -2,6 +2,8 @@ package com.example.moon.atapdetection;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -22,6 +24,9 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Math.abs;
 
@@ -31,39 +36,37 @@ import static java.lang.Math.abs;
 
 public class RealtimeDataDisplayFragment extends Fragment {
 
-    private LineGraphSeries<DataPoint> mSeries1;
-    private LineGraphSeries<DataPoint> mSeries2;
-    private LineGraphSeries<DataPoint> mSeries3;
-    private double graph2LastXValue = 0d;
-    private double previousDouble=0d;
-    private double previousDifferentiator=0d;
-    private double previousTap=0d;
-    private EditText etInfluence,etThreshold,etLag;
+    private LineGraphSeries<DataPoint> dataSignalLineGraphSeries;// add original signal to graph
+    private LineGraphSeries<DataPoint> binarySignalLineGraphSeries;//add peaks detected to graphs
+    private LineGraphSeries<DataPoint> stdeviationLineGraphSeries;// Standard deviation
+    private double graph2LastXValue = 0d;// X value of the Graph
+
+    private EditText etInfluence,etThreshold,etLag;// Edit texts
     private SeekBar seekBar;
      Sensor sensor;
      SensorManager sensorManager;
-    private  String binarySignal="";
-    private int samplingRate=5;
-    private  int sizeCounter=30;
-    private int initialTap=0;
-    private int tapGap=50;
-    ArrayList<Double> ternarySignals = new ArrayList<>();
-    ArrayList<Double> filteredY = new ArrayList<>();
-    ArrayList<Double> avgFilter = new ArrayList<>();
-    ArrayList<Double> stdFilter = new ArrayList<>();
-    ArrayList<Double> originalSignal = new ArrayList<>();
+    private  String binarySignal="";//String for matching with ending with 0100
+    private int samplingRate=5;//Current saples per second
 
-    int lag= 15;
-    int differentiatorSize=5;
-    double differentiatorArray[];
-    private  int i=lag;
-    double threshold= 3.5;
-    double influence= 0.0;
+    private int initialTap=0;// first tap detected at value
+    private int tapGap=50;// interval after which second tap is to be detected
 
-    double mean;
-    private boolean initialized=false;
+    ArrayList<Double> filteredY = new ArrayList<>();//initialised with values of original signal till lag th position
+    ArrayList<Double> avgFilter = new ArrayList<>();// moving average storage arraylist
+    ArrayList<Double> stdFilter = new ArrayList<>();// moving standard deviation storage arraylist
+    ArrayList<Double> originalSignal = new ArrayList<>();// original signal storage arraylist
+
+    int lag= 15;// window size to calculate mean
+
+    private  int i=lag;// x value counter,cannot be same xlastvalue as to initialize other variables
+    double threshold= 3.5;// Calculation of tap detection if signal is threshold values away
+    double influence= 0.5;// influence percentage of new signal to mean and standard deviation
+
+    double mean;// as the name suggests
+    private boolean initialized=false;// for values stored up to lag point
     private TextView textViewData,textViewSTD;
-    private EditText etTapGap;
+    private EditText etTapGap;// edit text to mannual enter tap gap
+    private LineGraphSeries<DataPoint> meanSeries;// mean sereis to add to graph
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,30 +78,46 @@ public class RealtimeDataDisplayFragment extends Fragment {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
 
-        differentiatorArray= new double[differentiatorSize];
-        for(int i=0;i<differentiatorSize;i++)
-            differentiatorArray[i]=0;
 
+/*------------------------------View Initializations and associating series to graphs------------------------------------*/
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        GraphView graph = (GraphView) rootView.findViewById(R.id.graph1);
-        mSeries1 = new LineGraphSeries<>();
-        graph.addSeries(mSeries1);
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graphView);
+        dataSignalLineGraphSeries = new LineGraphSeries<>();
+        graph.addSeries(dataSignalLineGraphSeries);
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(0);
         graph.getViewport().setMaxX(40);
+        GraphView graph2 = rootView.findViewById(R.id.graphBinary);
+        binarySignalLineGraphSeries = new LineGraphSeries<>();
+        graph2.addSeries(binarySignalLineGraphSeries);
+        graph2.getViewport().setXAxisBoundsManual(true);
+        graph2.getViewport().setMinX(0);
+        graph2.getViewport().setMaxX(40);
+        etInfluence= rootView.findViewById(R.id.editTextInfluence);
+        etLag=rootView.findViewById(R.id.editTextLag);
+        etThreshold=rootView.findViewById(R.id.editThreshold);
+        etTapGap=rootView.findViewById(R.id.editTextGap);
 
 
 
-
-
-
-
-
+        etLag.setOnKeyListener(onKeyListener);
+        etThreshold.setOnKeyListener(onKeyListener);
+        etInfluence.setOnKeyListener(onKeyListener);
+        etTapGap.setOnKeyListener(onKeyListener);
         seekBar= rootView.findViewById(R.id.seekBar);
+
+        stdeviationLineGraphSeries = new LineGraphSeries<>();
+        meanSeries = new LineGraphSeries<>();
+        graph.addSeries(stdeviationLineGraphSeries);
+        graph.addSeries(meanSeries);
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+        sensorManager.registerListener(sensorEventListener,sensor,samplingRate);
+
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                sensorManager.unregisterListener(sensorEventListener);
+                sensorManager.unregisterListener(sensorEventListener);// de reg
                 samplingRate=i;
                sensorManager.registerListener(sensorEventListener,sensor,samplingRate) ;
             }
@@ -113,33 +132,12 @@ public class RealtimeDataDisplayFragment extends Fragment {
 
             }
         });
-        GraphView graph2 = rootView.findViewById(R.id.graph2);
-        mSeries2 = new LineGraphSeries<>();
-        graph2.addSeries(mSeries2);
-        graph2.getViewport().setXAxisBoundsManual(true);
-        graph2.getViewport().setMinX(0);
-        graph2.getViewport().setMaxX(40);
-        etInfluence= rootView.findViewById(R.id.editTextInfluence);
-        etLag=rootView.findViewById(R.id.editTextLag);
-        etThreshold=rootView.findViewById(R.id.editThreshold);
-        etTapGap=rootView.findViewById(R.id.editTextGap);
-        textViewData=rootView.findViewById(R.id.textView);
-        textViewSTD=rootView.findViewById(R.id.textViewSTD);
-
-        etLag.setOnKeyListener(onKeyListener);
-        etThreshold.setOnKeyListener(onKeyListener);
-        etInfluence.setOnKeyListener(onKeyListener);
-        etTapGap.setOnKeyListener(onKeyListener);
-        GraphView graph3 =  rootView.findViewById(R.id.graphView);
 
 
 
-        mSeries3= new LineGraphSeries<>();
-        graph3.addSeries(mSeries3);
-        graph3.getViewport().setXAxisBoundsManual(true);
-        graph3.getViewport().setMinX(0);
-        graph3.getViewport().setMaxX(40);
-        sensorManager.registerListener(sensorEventListener,sensor,samplingRate);
+
+
+
 
 
 
@@ -147,7 +145,8 @@ public class RealtimeDataDisplayFragment extends Fragment {
         return rootView;
     }
 
-   SensorEventListener sensorEventListener=  new SensorEventListener() {
+    private int j=0;
+    SensorEventListener sensorEventListener=  new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
 
@@ -156,43 +155,50 @@ public class RealtimeDataDisplayFragment extends Fragment {
             float z = event.values[2];
             double total = Math.sqrt(x * x + y * y + z * z);
             //    double total=y;
-            double currentValue=  Math.round(total);
-            textViewData.setText( currentValue+"");
+            double currentValue=  total;
 
 
-            graph2LastXValue += 1d;
 
-            if(graph2LastXValue<lag){
+            graph2LastXValue += 1d;// increment x on graph
+
+            if(graph2LastXValue<lag){//if initialized uptill lag
 
                 originalSignal.add(currentValue);
                 filteredY.add(currentValue);
 
-            }else if(!initialized){
+            }else if(!initialized){//if lag = graph2lastvalue
                 originalSignal.add(currentValue);
                 filteredY.add(currentValue);
-                setZeros();
-                mean = mean(originalSignal, lag, 0);
-                avgFilter.add(lag, mean);
-                stdFilter.add( lag,standardDeviation(originalSignal, mean, lag, 0));
+                setZeros();// setting zeros to required array lists
+                mean = mean(originalSignal, lag, 0);// initial mean till lag window
+                avgFilter.add(lag, mean);//add mean to arraylist
+                stdFilter.add( lag,standardDeviation(originalSignal, mean, lag, 0));//add stdeviation to arraylist
 
                 initialized=true;
             }
-            else
+            else// always execute after initialization complete
             {
                 originalSignal.add(currentValue);
+
                 method();
             }
 
-            mSeries1.appendData(new DataPoint(graph2LastXValue, currentValue), true, 40);
+
+          /*---------------------------Adding Data To graph-------------------------------------------*/
+            dataSignalLineGraphSeries.setColor(Color.GRAY);
+            dataSignalLineGraphSeries.appendData(new DataPoint(graph2LastXValue, currentValue), true, 40);
+
+            if(!stdFilter.isEmpty()) {
+                stdeviationLineGraphSeries.setColor(Color.BLUE);
+                stdeviationLineGraphSeries.appendData(new DataPoint(graph2LastXValue,threshold* -stdFilter.get(j)), true, 40);
+            stdeviationLineGraphSeries.appendData(new DataPoint(graph2LastXValue,threshold* stdFilter.get(j)), true, 40);}
+            if(!avgFilter.isEmpty()) {
+                meanSeries.setColor(Color.GREEN);
+                meanSeries.appendData(new DataPoint(graph2LastXValue, avgFilter.get(j)), true, 40);
+            }
 
 
-
-
-
-
-
-
-
+/*-------------------------------------------------------------------------------------------------------*/
 
         }
 
@@ -201,7 +207,7 @@ public class RealtimeDataDisplayFragment extends Fragment {
 
 
     }};
-
+/*---------------------------------------------Adding LIstener to  edit texts for manually changing of threshold,lag,influence ,and tap interval values----------------------------------*/
         View.OnKeyListener onKeyListener = new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
@@ -226,6 +232,9 @@ public class RealtimeDataDisplayFragment extends Fragment {
                             break;
 
                         case R.id.editTextGap:
+
+
+
                     }
 
 
@@ -236,7 +245,19 @@ public class RealtimeDataDisplayFragment extends Fragment {
                 return false;
             }
         };
+/*---------------------------------------------------------------------------------------------------------------------*/
 
+
+
+
+
+
+
+
+
+
+
+/*-----------------------------------Zscore threshlding Algorithm ----------------------------------------------------------*/
     public  void method() {
 
 
@@ -244,51 +265,64 @@ public class RealtimeDataDisplayFragment extends Fragment {
         try {
 
 
-                if (abs(originalSignal.get(i) - avgFilter.get(i - 1)) > threshold * stdFilter.get(i-1)) {
+                if (abs(originalSignal.get(i) - avgFilter.get(i - 1)) > threshold * stdFilter.get(i-1)) {// check if the diference from mean is greater than threshold times standard deviation of previous
                     if (originalSignal.get(i) > avgFilter.get(i - 1)) {
-                       ternarySignals.add( 1.0);
+
                         binarySignal=binarySignal+1;
-                        mSeries2.appendData(new DataPoint(graph2LastXValue, 1.0), true, 40);
+                        binarySignalLineGraphSeries.appendData(new DataPoint(graph2LastXValue, 1.0), true, 40);// add to realtime graph
                     } else {
-                        ternarySignals.add( -1.0);
-                        mSeries2.appendData(new DataPoint(graph2LastXValue, -1.0), true, 40);
+
+                        binarySignalLineGraphSeries.appendData(new DataPoint(graph2LastXValue, -1.0), true, 40);//add to realtime graph
 
                     }
-                    filteredY.add(influence * originalSignal.get(i) + (1 - influence) * filteredY.get(i-1));
+                    filteredY.add(influence * originalSignal.get(i) + (1 - influence) * filteredY.get(i-1));// influence role: How much does the standard deviation and mean is affected by newer values of the the signal from accelerometer
                 } else {
-                    ternarySignals.add( 0.0);
-                    binarySignal=binarySignal+0;
-                    mSeries2.appendData(new DataPoint(graph2LastXValue, 0.0), true, 40);
+
+                    binarySignal=binarySignal+0;// this section for no change in values
+                    binarySignalLineGraphSeries.appendData(new DataPoint(graph2LastXValue, 0.0), true, 40);//add to realtime graph
 
                     filteredY.add( originalSignal.get(i));
                 }
                     Log.d("I :",i+"");
-            Log.d("Binary :",binarySignal);
+          //  Log.d("Binary :",binarySignal);
                 mean = mean(filteredY, i, i - lag);
                 avgFilter.add( mean);
+
+
             double std=standardDeviation(filteredY, mean, i, i - lag);
+            Log.d("Data :","Mean: "+ mean+"STD : "+std *threshold);
+
                 stdFilter.add( std);
+            j++;
 
             i++;
-            if(binarySignal.endsWith("0100") ) {
-                if( initialTap==0)
-                initialTap=i;
-                else if(i>=initialTap+tapGap) {
-                    Toast.makeText(getContext(), "Double Tap ", Toast.LENGTH_SHORT).show();
+
+            /*---------------------------Taps detection logic-------------------------------------*/
+            if(binarySignal.endsWith("0100") ) {// Regex matching with ends with to detect tap
+
+                if(initialTap==0){ // first tap detect and hold position
+                    initialTap=i;
+                    binarySignal="";
+
+
+
+                }
+                else if (i>initialTap+tapGap) {//Check if second tap detected in signals
+
+                    initialTap=0;
+                    binarySignal="";
+                    OnDoubleTapDetection();
+                }
+
+                else {//  Second tap not detected then declare single tap
+                    OnSingleTapDetection();
                     initialTap=0;
                     binarySignal="";
                 }
-                else{
-                    Toast.makeText(getContext(), "Single Tap ", Toast.LENGTH_SHORT).show();
-                    initialTap=0;
-                    binarySignal="";
                 }
 
-
-            }
-
-
-
+ /*------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
@@ -302,6 +336,12 @@ public class RealtimeDataDisplayFragment extends Fragment {
 
     }
 
+
+
+
+
+
+
  public void   setZeros(){
 
      for(int i=0;i<lag;i++){
@@ -312,7 +352,7 @@ public class RealtimeDataDisplayFragment extends Fragment {
 
     }
 
-
+/*-----------Calculating Standard deviation function-----------------------------------------*/
     private double standardDeviation(ArrayList<Double> signal,double mean,int upperLimit,int lowerLimit){
 
 
@@ -329,7 +369,7 @@ public class RealtimeDataDisplayFragment extends Fragment {
     }
 
 
-
+/*-----------Calculating Mean function-----------------------------------------*/
 
     private double mean(ArrayList<Double> signal ,int upperLimit,int lowerLimit){
 
@@ -359,13 +399,21 @@ public class RealtimeDataDisplayFragment extends Fragment {
 
         super.onPause();
     }
+    void OnSingleTapDetection(){
+        Toast.makeText(getContext(),"Single",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent("com.android.music.musicservicecommand.togglepause");
+        i.putExtra("command", "next");
+        getContext().sendBroadcast(i);
+    }
+    void OnDoubleTapDetection(){
+        Toast.makeText(getContext(),"Double",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent("com.android.music.musicservicecommand.togglepause");
+        i.putExtra("command", "togglepause");
+        getContext().sendBroadcast(i);
 
-public interface  TapDetection{
 
-    void OnSingleTapDetection();
-    void OnDoubleTapDetection();
+    }
 
-}
 
 
 
